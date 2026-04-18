bc90331531ef2750912ec1943270578c426825b4
Gabriell3301 <gabriellrito@gmail.com>
2026-04-19 00:03:57 +0100

    Criando a DB e colocando a tabela Utilizador (Está em branco ainda)

---
+++ AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_DA
+{
+    internal class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("AppDbContext")
+        {
+        }
+        public DbSet<Utilizador> Utilizadores { get; set; }
+    }
+}
