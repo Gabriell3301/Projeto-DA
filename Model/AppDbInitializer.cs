ba6d7161f4d00fc66570c3c5af3470f4690473be
Gabriell3301 <gabriellrito@gmail.com>
2026-05-07 11:51:54 +0100

    Criando um inicializador da Db com o login de Admin

---
+++ Model/AppDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Projeto_DA.Model
+{
+    internal class AppDbInitializer : DropCreateDatabaseIfModelChanges<AppDbContext>
+    {
+        protected override void Seed(AppDbContext context)
+        {
+            context.Utilizadores.Add(new Utilizador
+            {
+                Nome = "Admin",
+                Username = "admin",
+                Password = "admin123" // In a real application, use hashed passwords
+            });
+            // Seed initial data if necessary
+            base.Seed(context);
+        }
+    }
+}
