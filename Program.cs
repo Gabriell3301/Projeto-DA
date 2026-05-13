a114db7a0fa93d07ad1c1b84dd256f47e35efdbc
Gabriell3301 <gabriellrito@gmail.com>
2026-05-13 12:18:31 +0100

    feat(Database): configurar inicialização da base de dados no Program.cs e carregamento de utilizadores (login)

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.Entity;
 using System.Windows.Forms;
+using Projeto_DA.Model;
 
 namespace Projeto_DA
 {
@@ -14,10 +16,18 @@ namespace Projeto_DA
         [STAThread]
         static void Main()
         {
+            Database.SetInitializer(new AppDbInitializer());
+            using (AppDbContext db = new AppDbContext())
+            {
+                db.Database.Initialize(false);
+            }
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             if (new LoginPage().ShowDialog() == DialogResult.OK)
             {
+                MessageBox.Show("Login bem sucedido!");
                 //Aqui entra o forms de pagina inicial que ainda não existe.
             }
         }
