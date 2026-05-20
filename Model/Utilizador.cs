c283ac665a0543691a08fe81a6249af18abe4fb9
Gabriell3301 <gabriellrito@gmail.com>
2026-05-20 11:27:29 +0100

    Tirando espaços

--- Model/Utilizador.cs
+++ Model/Utilizador.cs
@@ -10,7 +10,6 @@ namespace Projeto_DA
     {
         public int Id { get; set; }
 
-
         public string Nome { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
