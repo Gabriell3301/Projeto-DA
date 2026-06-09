c116a58d5b164d13ccf2e1b830621360c3056e8c
Gabriell3301 <gabriellrito@gmail.com>
2026-06-09 15:34:51 +0100

    Adicionando o Forms para adicionar os tipos de artigos

---
+++ View/TipoArtigoForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Projeto_DA.View
+{
+    partial class TipoArtigoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTiposArtigos = new System.Windows.Forms.DataGridView();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTiposArtigos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTiposArtigos
+            // 
+            this.dgvTiposArtigos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTiposArtigos.Location = new System.Drawing.Point(24, 12);
+            this.dgvTiposArtigos.Name = "dgvTiposArtigos";
+            this.dgvTiposArtigos.Size = new System.Drawing.Size(240, 150);
+            this.dgvTiposArtigos.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(86, 168);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(24, 209);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(189, 209);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(24, 253);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(189, 253);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // TipoArtigoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.dgvTiposArtigos);
+            this.Name = "TipoArtigoForm";
+            this.Text = "TipoArtigoForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTiposArtigos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTiposArtigos;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
---
+++ View/TipoArtigoForm.cs
@@ -0,0 +1,108 @@
+﻿using Projeto_DA.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_DA.View
+{
+    public partial class TipoArtigoForm : Form
+    {
+        public TipoArtigoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CarregarDados()
+        {
+            using (var db = new AppDbContext())
+            {
+                var tipos = db.TiposArtigos.ToList();
+                dgvTiposArtigos.DataSource = tipos;
+            }
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txtNome.Text))
+            {
+                MessageBox.Show("Insira um Nome para o novo tipo de artigo.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            using (var db = new AppDbContext())
+            {
+                TipoArtigo tipo = new TipoArtigo()
+                {
+                    Nome = txtNome.Text
+                };
+                db.TiposArtigos.Add(tipo);
+                db.SaveChanges();
+            }
+            MessageBox.Show("Tipo de Artigo criado com sucesso.", "Sucesso");
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dgvTiposArtigos.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecione/Clique em um tipo de artigo.");
+                return;
+            }
+
+            int id = Convert.ToInt32(
+                dgvTiposArtigos.SelectedRows[0].Cells["Id"].Value);
+
+            FormEditarTipoArtigo form = new FormEditarTipoArtigo(id);
+
+            form.ShowDialog();
+
+            CarregarDados();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvTiposArtigos.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecione/Clique em um tipo de artigo.");
+                return;
+            }
+            int id = Convert.ToInt32(
+                dgvTiposArtigos.SelectedRows[0].Cells["Id"].Value);
+
+            DialogResult resposta = MessageBox.Show(
+               "Tem a certeza que deseja eliminar este tipo de artigo?",
+               "Confirmação",
+               MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question);
+
+            if (resposta == DialogResult.No)
+                return;
+
+            using (var db = new AppDbContext())
+            {
+                TipoArtigo tipo = db.TiposArtigos.Find(id);
+
+                if (tipo == null)
+                {
+                    MessageBox.Show("Tipo de artigo não encontrado.");
+                    return;
+                }
+
+                db.TiposArtigos.Remove(tipo);
+
+                db.SaveChanges();
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            dgvTiposArtigos.ClearSelection();
+            txtNome.Clear();
+        }
+    }
+}
