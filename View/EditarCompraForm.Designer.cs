6ab53d0a5e03456f2d3620dacd1ef4a01948b5aa
Gabriell3301 <gabriellrito@gmail.com>
2026-06-09 21:18:06 +0100

    Criando o editar compra

---
+++ View/EditarCompraForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Projeto_DA.View
+{
+    partial class EditarCompraForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dataCriacao = new System.Windows.Forms.Label();
+            this.utilizadorCriacao = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmbTipoArtigo = new System.Windows.Forms.ComboBox();
+            this.cmbArtigo = new System.Windows.Forms.ComboBox();
+            this.numQuantidade = new System.Windows.Forms.NumericUpDown();
+            this.btnAdicionarItem = new System.Windows.Forms.Button();
+            this.btnRemoverItem = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoArtigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Artigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QtdPrevista = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecoUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantidade)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(332, 175);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(123, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // dataCriacao
+            // 
+            this.dataCriacao.AutoSize = true;
+            this.dataCriacao.Location = new System.Drawing.Point(105, 178);
+            this.dataCriacao.Name = "dataCriacao";
+            this.dataCriacao.Size = new System.Drawing.Size(65, 13);
+            this.dataCriacao.TabIndex = 1;
+            this.dataCriacao.Text = "01/01/2000";
+            this.dataCriacao.Click += new System.EventHandler(this.dataCriacao_Click);
+            // 
+            // utilizadorCriacao
+            // 
+            this.utilizadorCriacao.AutoSize = true;
+            this.utilizadorCriacao.Location = new System.Drawing.Point(232, 178);
+            this.utilizadorCriacao.Name = "utilizadorCriacao";
+            this.utilizadorCriacao.Size = new System.Drawing.Size(42, 13);
+            this.utilizadorCriacao.TabIndex = 2;
+            this.utilizadorCriacao.Text = "Gabriell";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.TipoArtigo,
+            this.Artigo,
+            this.QtdPrevista,
+            this.PrecoUnitario});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // cmbTipoArtigo
+            // 
+            this.cmbTipoArtigo.FormattingEnabled = true;
+            this.cmbTipoArtigo.Location = new System.Drawing.Point(94, 198);
+            this.cmbTipoArtigo.Name = "cmbTipoArtigo";
+            this.cmbTipoArtigo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoArtigo.TabIndex = 4;
+            this.cmbTipoArtigo.SelectedIndexChanged += new System.EventHandler(this.cmbTipoArtigo_SelectedIndexChanged);
+            // 
+            // cmbArtigo
+            // 
+            this.cmbArtigo.FormattingEnabled = true;
+            this.cmbArtigo.Location = new System.Drawing.Point(58, 221);
+            this.cmbArtigo.Name = "cmbArtigo";
+            this.cmbArtigo.Size = new System.Drawing.Size(121, 21);
+            this.cmbArtigo.TabIndex = 5;
+            // 
+            // numQuantidade
+            // 
+            this.numQuantidade.Location = new System.Drawing.Point(260, 221);
+            this.numQuantidade.Name = "numQuantidade";
+            this.numQuantidade.Size = new System.Drawing.Size(120, 20);
+            this.numQuantidade.TabIndex = 6;
+            // 
+            // btnAdicionarItem
+            // 
+            this.btnAdicionarItem.Location = new System.Drawing.Point(108, 272);
+            this.btnAdicionarItem.Name = "btnAdicionarItem";
+            this.btnAdicionarItem.Size = new System.Drawing.Size(75, 34);
+            this.btnAdicionarItem.TabIndex = 7;
+            this.btnAdicionarItem.Text = "Adicionar Item";
+            this.btnAdicionarItem.UseVisualStyleBackColor = true;
+            this.btnAdicionarItem.Click += new System.EventHandler(this.btnAdicionarItem_Click);
+            // 
+            // btnRemoverItem
+            // 
+            this.btnRemoverItem.Location = new System.Drawing.Point(189, 272);
+            this.btnRemoverItem.Name = "btnRemoverItem";
+            this.btnRemoverItem.Size = new System.Drawing.Size(75, 34);
+            this.btnRemoverItem.TabIndex = 8;
+            this.btnRemoverItem.Text = "Remover Item";
+            this.btnRemoverItem.UseVisualStyleBackColor = true;
+            this.btnRemoverItem.Click += new System.EventHandler(this.btnRemoverItem_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(270, 272);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 35);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar Compra";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // TipoArtigo
+            // 
+            this.TipoArtigo.HeaderText = "Tipo Artigo";
+            this.TipoArtigo.Name = "TipoArtigo";
+            // 
+            // Artigo
+            // 
+            this.Artigo.HeaderText = "Artigo";
+            this.Artigo.Name = "Artigo";
+            // 
+            // QtdPrevista
+            // 
+            this.QtdPrevista.HeaderText = "Qtd. Prevista";
+            this.QtdPrevista.Name = "QtdPrevista";
+            // 
+            // PrecoUnitario
+            // 
+            this.PrecoUnitario.HeaderText = "Preço Unitário";
+            this.PrecoUnitario.Name = "PrecoUnitario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Data de Criação: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Utilizador: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(280, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Compra:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Tipo do Artigo: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Artigo: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(189, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Quantidade:";
+            // 
+            // EditarCompraForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 333);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnRemoverItem);
+            this.Controls.Add(this.btnAdicionarItem);
+            this.Controls.Add(this.numQuantidade);
+            this.Controls.Add(this.cmbArtigo);
+            this.Controls.Add(this.cmbTipoArtigo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.utilizadorCriacao);
+            this.Controls.Add(this.dataCriacao);
+            this.Controls.Add(this.txtNome);
+            this.Name = "EditarCompraForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EditarCompraForm";
+            this.Load += new System.EventHandler(this.EditarCompraForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantidade)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label dataCriacao;
+        private System.Windows.Forms.Label utilizadorCriacao;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbTipoArtigo;
+        private System.Windows.Forms.ComboBox cmbArtigo;
+        private System.Windows.Forms.NumericUpDown numQuantidade;
+        private System.Windows.Forms.Button btnAdicionarItem;
+        private System.Windows.Forms.Button btnRemoverItem;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoArtigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Artigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QtdPrevista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrecoUnitario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ View/EditarCompraForm.cs
@@ -0,0 +1,289 @@
+﻿using Projeto_DA.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_DA.View
+{
+    public partial class EditarCompraForm : Form
+    {
+        int _compraId;
+        Utilizador _utilizadorLogado;
+        bool _fechada;
+        Compra _compraAtual; // Guardamos a compra em memória para manipular os Itens
+
+        public EditarCompraForm(int compraId, Utilizador utilizadorLogado, bool fechada)
+        {
+            InitializeComponent();
+            _compraId = compraId;
+            _utilizadorLogado = utilizadorLogado;
+            _fechada = fechada;
+
+        }
+
+        private void EditarCompraForm_Load(object sender, EventArgs e)
+        {
+            CarregarArtigosComboBox();
+            CarregarDadosCompra();
+
+            if (_fechada)
+            {
+                btnAdicionarItem.Enabled = false;
+                btnRemoverItem.Enabled = false;
+                btnGuardar.Enabled = false;
+                cmbTipoArtigo.Enabled = false;
+                cmbArtigo.Enabled = false;
+                MessageBox.Show ("Esta compra já está fechada. Não pode ser modificada.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning); // Opcional, se tiveres uma label de aviso
+            }
+        }
+        private void CarregarArtigosComboBox()
+        {
+            using (var db = new AppDbContext())
+            {
+                var tipos = db.TiposArtigos.ToList();
+
+                cmbTipoArtigo.DisplayMember = "Nome";
+                cmbTipoArtigo.ValueMember = "Id";
+                cmbTipoArtigo.DataSource = tipos; // Ligar a DataSource no fim evita bugs no evento!
+            }
+        }
+
+        private void CarregarDadosCompra()
+        {
+            using (var db = new AppDbContext())
+            {
+                // O Include em cadeia carrega a Compra -> os Itens -> o Artigo de cada item -> o Tipo de cada artigo
+                _compraAtual = db.Compras
+                    .Include("Itens")
+                    .Include("Itens.Artigo")
+                    .Include("Itens.Artigo.TipoArtigo")
+                    .FirstOrDefault(c => c.Id == _compraId);
+
+                if (_compraAtual == null)
+                {
+                    MessageBox.Show("Compra não encontrada!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                    return;
+                }
+
+                // Atualiza os textos do teu formulário
+                txtNome.Text = _compraAtual.Nome;
+                dataCriacao.Text = _compraAtual.DataCriacao.ToString("dd/MM/yyyy");
+
+                // Chama o método que acabámos de corrigir
+                AtualizarGridItens();
+            }
+        }
+        private void AtualizarGridItens()
+        {
+            dataGridView1.Rows.Clear();
+
+            using (var db = new AppDbContext())
+            {
+                foreach (var item in _compraAtual.Itens)
+                {
+                    string nomeTipo = "N/A";
+
+                    if (item.Artigo != null)
+                    {
+                        var tipo = db.TiposArtigos
+                            .FirstOrDefault(t => t.Id == item.Artigo.TipoArtigoId);
+
+                        if (tipo != null)
+                            nomeTipo = tipo.Nome;
+                    }
+
+                    string nomeArtigo = item.Artigo?.Nome ?? "Desconhecido";
+
+                    dataGridView1.Rows.Add(
+                        item.Id,
+                        nomeTipo,
+                        nomeArtigo,
+                        item.QuantidadePrevista,
+                        item.PrecoUnitario.ToString("F2")
+                    );
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataCriacao_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdicionarItem_Click(object sender, EventArgs e)
+        {
+            if (cmbArtigo.SelectedValue == null) return;
+
+            int artigoId = Convert.ToInt32(cmbArtigo.SelectedValue);
+            int quantidade = (int)numQuantidade.Value; // Ajusta para o nome do teu controlo de quantidade
+
+            if (quantidade <= 0)
+            {
+                MessageBox.Show("A quantidade deve ser maior que zero.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Verificar se o artigo já existe nesta lista de planeamento para não duplicar linhas do mesmo artigo
+            var itemExistente = _compraAtual.Itens.FirstOrDefault(i => i.ArtigoId == artigoId);
+            if (itemExistente != null)
+            {
+                itemExistente.QuantidadePrevista += quantidade;
+            }
+            else
+            {
+                // Criar o novo item de planeamento
+                var novoItem = new ItemCompra
+                {
+                    CompraId = _compraId,
+                    ArtigoId = artigoId,
+                    QuantidadePrevista = quantidade,
+                    EhPrevisto = true, // No planeamento todos são previstos
+                    QuantidadeReal = 0,
+                    PrecoUnitario = 0
+                };
+
+                // Como o include do EF traz objetos associados, vamos buscar o Artigo para ele aparecer na Grid antes de gravares
+                using (var db = new AppDbContext())
+                {
+                    novoItem.Artigo = db.Artigos.Find(artigoId);
+                }
+
+                _compraAtual.Itens.Add(novoItem);
+            }
+
+            AtualizarGridItens();
+        }
+
+        private void btnRemoverItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+
+            // Vamos buscar o ArtigoId ou o Id do Item da linha selecionada. 
+            // Como os itens novos em memória não têm ID da BD (é 0), o mais seguro é buscar pelo Nome do Artigo ou correspondência direta.
+            string nomeArtigo = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+
+            var itemParaRemover = _compraAtual.Itens.FirstOrDefault(i => i.Artigo?.Nome == nomeArtigo);
+
+            if (itemParaRemover != null)
+            {
+                _compraAtual.Itens.Remove(itemParaRemover);
+
+                // Se o item já tinha ID da Base de Dados, vais ter de o remover explicitamente no botão Guardar.
+                // Mas para a lista em memória, basta o Remove().
+
+                AtualizarGridItens();
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var db = new AppDbContext())
+                {
+                    // Forçamos o Entity Framework a seguir a compra original da BD
+                    var compraBD = db.Compras.Include("Itens").FirstOrDefault(c => c.Id == _compraId);
+
+                    if (compraBD == null) return;
+
+                    // 1. Remover da BD os itens que o utilizador apagou na interface
+                    var itensParaApagar = compraBD.Itens
+                        .Where(dbItem => !_compraAtual.Itens.Any(memItem => memItem.Id == dbItem.Id))
+                        .ToList();
+
+                    foreach (var item in itensParaApagar)
+                    {
+                        db.ItensCompra.Remove(item); // Ajusta para o nome do teu DbSet de itens se for diferente (ex: db.ItensCompra)
+                    }
+
+                    // 2. Atualizar ou Adicionar os itens atuais
+                    foreach (var memItem in _compraAtual.Itens)
+                    {
+                        if (memItem.Id == 0)
+                        {
+                            // É um item novo, adicionamos à lista da BD
+                            compraBD.Itens.Add(new ItemCompra
+                            {
+                                ArtigoId = memItem.ArtigoId,
+                                QuantidadePrevista = memItem.QuantidadePrevista,
+                                EhPrevisto = true,
+                                QuantidadeReal = 0,
+                                PrecoUnitario = 0
+                            });
+                        }
+                        else
+                        {
+                            // Item já existia, atualizamos apenas a quantidade planeada
+                            var dbItem = compraBD.Itens.FirstOrDefault(i => i.Id == memItem.Id);
+                            if (dbItem != null)
+                            {
+                                dbItem.QuantidadePrevista = memItem.QuantidadePrevista;
+                            }
+                        }
+                    }
+
+                    db.SaveChanges();
+                }
+
+                MessageBox.Show("Planeamento guardado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close(); // Fecha o ecrã e volta ao menu principal
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao guardar: {ex.Message}", "Erro Crítico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cmbTipoArtigo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Se não houver nada selecionado, sai
+            if (cmbTipoArtigo.SelectedValue == null)
+                return;
+
+            int tipoId = 0;
+
+            // Se o WinForms enviar o objeto completo (comum no Load), extraímos o ID dele
+            if (cmbTipoArtigo.SelectedValue is TipoArtigo tipoSelecionado)
+            {
+                tipoId = tipoSelecionado.Id;
+            }
+            // Se já for o ID propriamente dito (inteiro), fazemos a conversão direta
+            else if (cmbTipoArtigo.SelectedValue is int idInteiro)
+            {
+                tipoId = idInteiro;
+            }
+            else
+            {
+                return; // Caso seja outro tipo inesperado, ignora
+            }
+
+            using (var db = new AppDbContext())
+            {
+                // Corrigido o espaço: de 'artigos Filtrados' para 'artigosFiltrados'
+                // Confirmado que na tua classe Artigo o campo se chama exatamente TipoArtigoId
+                var artigosFiltrados = db.Artigos
+                    .Where(a => a.TipoArtigoId == tipoId)
+                    .ToList();
+
+                // Desvincula temporariamente para evitar loops de eventos e limpa
+                cmbArtigo.DataSource = null;
+
+                cmbArtigo.DataSource = artigosFiltrados;
+                cmbArtigo.DisplayMember = "Nome";
+                cmbArtigo.ValueMember = "Id";
+            }
+        }
+    }
+}
