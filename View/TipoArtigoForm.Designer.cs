e0f6bdda35fb146eeed60d925b999d551cd3aeda
Gabriell3301 <gabriellrito@gmail.com>
2026-06-09 19:10:04 +0100

    Ajustando o tamanho da tela do Tipo de artigo

--- View/TipoArtigoForm.Designer.cs
+++ View/TipoArtigoForm.Designer.cs
@@ -96,7 +96,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(287, 303);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnEditar);
