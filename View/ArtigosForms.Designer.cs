604eff19333a804023976e55a9c73458bc0cbfc4
Gabriell3301 <gabriellrito@gmail.com>
2026-06-09 16:25:11 +0100

    Forms para criação de artigos novos

---
+++ View/ArtigosForms.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Projeto_DA.View
+{
+    partial class ArtigosForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbTipoArtigo = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtNomeArtigo = new System.Windows.Forms.TextBox();
+            this.cmbTipoArtigoNovo = new System.Windows.Forms.ComboBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoArtigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbTipoArtigo
+            // 
+            this.cmbTipoArtigo.FormattingEnabled = true;
+            this.cmbTipoArtigo.Location = new System.Drawing.Point(12, 177);
+            this.cmbTipoArtigo.Name = "cmbTipoArtigo";
+            this.cmbTipoArtigo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoArtigo.TabIndex = 0;
+            this.cmbTipoArtigo.SelectedIndexChanged += new System.EventHandler(this.cmbTipoArtigo_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nome,
+            this.TipoArtigo});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(248, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtNomeArtigo
+            // 
+            this.txtNomeArtigo.Location = new System.Drawing.Point(88, 214);
+            this.txtNomeArtigo.Name = "txtNomeArtigo";
+            this.txtNomeArtigo.Size = new System.Drawing.Size(100, 20);
+            this.txtNomeArtigo.TabIndex = 2;
+            // 
+            // cmbTipoArtigoNovo
+            // 
+            this.cmbTipoArtigoNovo.FormattingEnabled = true;
+            this.cmbTipoArtigoNovo.Location = new System.Drawing.Point(139, 177);
+            this.cmbTipoArtigoNovo.Name = "cmbTipoArtigoNovo";
+            this.cmbTipoArtigoNovo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoArtigoNovo.TabIndex = 3;
+            this.cmbTipoArtigoNovo.SelectedIndexChanged += new System.EventHandler(this.cmbTipoArtigoNovo_SelectedIndexChanged);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(49, 251);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 4;
+            this.btnAdicionar.Text = "Adcionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(49, 301);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(149, 251);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(149, 301);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // TipoArtigo
+            // 
+            this.TipoArtigo.HeaderText = "TipoArtigo";
+            this.TipoArtigo.Name = "TipoArtigo";
+            // 
+            // ArtigosForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 348);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.cmbTipoArtigoNovo);
+            this.Controls.Add(this.txtNomeArtigo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbTipoArtigo);
+            this.Name = "ArtigosForms";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ArtigosForms";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbTipoArtigo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNomeArtigo;
+        private System.Windows.Forms.ComboBox cmbTipoArtigoNovo;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoArtigo;
+    }
+}
---
+++ View/ArtigosForms.cs
@@ -0,0 +1,154 @@
+﻿using Projeto_DA.Classes;
+using System;
+using System.Data.Entity;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Projeto_DA.View
+{
+    public partial class ArtigosForms : Form
+    {
+        private int? _editandoId = null;
+        public ArtigosForms()
+        {
+            InitializeComponent();
+            CarregarTipos();
+            CarregarArtigos();
+        }
+
+        private void CarregarTipos()
+        {
+            using (var db = new AppDbContext())
+            {
+                // Filtro
+                cmbTipoArtigo.Items.Clear();
+                cmbTipoArtigo.Items.Add("Todos");
+                foreach (var t in db.TiposArtigos.ToList())
+                    cmbTipoArtigo.Items.Add(t);
+                cmbTipoArtigo.DisplayMember = "Nome";
+                cmbTipoArtigo.SelectedIndex = 0;
+
+                // ComboBox do formulário
+                cmbTipoArtigoNovo.DataSource = db.TiposArtigos.ToList();
+                cmbTipoArtigoNovo.DisplayMember = "Nome";
+                cmbTipoArtigoNovo.ValueMember = "Id";
+            }
+        }
+
+        private void CarregarArtigos()
+        {
+            using (var db = new AppDbContext())
+            {
+                var query = db.Artigos.Include(a => a.TipoArtigo).AsQueryable();
+
+                if (cmbTipoArtigo.SelectedItem is TipoArtigo tipo)
+                    query = query.Where(a => a.TipoArtigoId == tipo.Id);
+
+                dataGridView1.Rows.Clear();
+                foreach (var a in query.ToList())
+                    dataGridView1.Rows.Add(a.Id, a.Nome, a.TipoArtigo?.Nome);
+            }
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNomeArtigo.Text) || cmbTipoArtigoNovo.SelectedItem == null)
+            {
+                MessageBox.Show("Preencha todos os campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (var db = new AppDbContext())
+                {
+                    if (_editandoId == null)
+                    {
+                        db.Artigos.Add(new Artigo
+                        {
+                            Nome = txtNomeArtigo.Text.Trim(),
+                            TipoArtigoId = (int)cmbTipoArtigoNovo.SelectedValue
+                        });
+                    }
+                    else
+                    {
+                        var artigo = db.Artigos.Find(_editandoId);
+                        artigo.Nome = txtNomeArtigo.Text.Trim();
+                        artigo.TipoArtigoId = (int)cmbTipoArtigoNovo.SelectedValue;
+                    }
+                    db.SaveChanges();
+                }
+                Limpar();
+                CarregarArtigos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+
+            _editandoId = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            txtNomeArtigo.Text = dataGridView1.CurrentRow.Cells[1].Value?.ToString();
+
+            string nomeTipo = dataGridView1.CurrentRow.Cells[2].Value?.ToString();
+            foreach (TipoArtigo t in cmbTipoArtigoNovo.Items)
+                if (t.Nome == nomeTipo) { cmbTipoArtigoNovo.SelectedItem = t; break; }
+
+            btnAdicionar.Text = "Guardar";
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+
+            if (MessageBox.Show("Eliminar este artigo?", "Confirmar",
+                MessageBoxButtons.YesNo) != DialogResult.Yes) return;
+
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            try
+            {
+                using (var db = new AppDbContext())
+                {
+                    var artigo = db.Artigos.Find(id);
+                    db.Artigos.Remove(artigo);
+                    db.SaveChanges();
+                }
+                Limpar();
+                CarregarArtigos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e) => Limpar();
+
+        private void cmbTipoArtigo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CarregarArtigos();
+        }
+
+        private void cmbTipoArtigoNovo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void Limpar()
+        {
+            txtNomeArtigo.Text = "";
+            cmbTipoArtigoNovo.SelectedIndex = -1;
+            _editandoId = null;
+            btnAdicionar.Text = "Adicionar";
+        }
+    }
+}
