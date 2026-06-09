7c456bbb43ba01211e3f37a299d4224d3d4744b7
Gabriell3301 <gabriellrito@gmail.com>
2026-06-09 19:09:25 +0100

    Formulário para criação de utilizadores

---
+++ View/UtilizadoresForms.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Projeto_DA.View
+{
+    partial class UtilizadoresForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtUtilizador = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Utilizador = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Senha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nome,
+            this.Utilizador,
+            this.Senha});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(332, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(16, 190);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtUtilizador
+            // 
+            this.txtUtilizador.Location = new System.Drawing.Point(131, 190);
+            this.txtUtilizador.Name = "txtUtilizador";
+            this.txtUtilizador.Size = new System.Drawing.Size(100, 20);
+            this.txtUtilizador.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(248, 190);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 3;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(25, 216);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 4;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(143, 222);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(261, 216);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(143, 260);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nome: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Utilizador: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Senha: ";
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.Visible = false;
+            // 
+            // Nome
+            // 
+            this.Nome.HeaderText = "Nome";
+            this.Nome.Name = "Nome";
+            // 
+            // Utilizador
+            // 
+            this.Utilizador.HeaderText = "Utilizador";
+            this.Utilizador.Name = "Utilizador";
+            // 
+            // Senha
+            // 
+            this.Senha.HeaderText = "Senha";
+            this.Senha.Name = "Senha";
+            // 
+            // UtilizadoresForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 295);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUtilizador);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "UtilizadoresForms";
+            this.Text = "UtilizadoresForms";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtUtilizador;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Utilizador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Senha;
+    }
+}
---
+++ View/UtilizadoresForms.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_DA.View
+{
+    public partial class UtilizadoresForms : Form
+    {
+        private int? _editandoId = null;
+        public UtilizadoresForms()
+        {
+            InitializeComponent();
+            CarregarUtilizadores();
+        }
+
+        private void CarregarUtilizadores()
+        {
+            using (var db = new AppDbContext())
+            {
+                dataGridView1.Rows.Clear();
+                foreach (var u in db.Utilizadores.ToList())
+                    dataGridView1.Rows.Add(u.Id, u.Nome, u.Username, "****");
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e) => Limpar();
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNome.Text) ||
+                string.IsNullOrWhiteSpace(txtUtilizador.Text) ||
+                string.IsNullOrWhiteSpace(txtSenha.Text))
+            {
+                MessageBox.Show("Preencha todos os campos.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (var db = new AppDbContext())
+                {
+                    // Verificar username único
+                    bool existe = db.Utilizadores.Any(u =>
+                        u.Username == txtUtilizador.Text && u.Id != (_editandoId ?? 0));
+
+                    if (existe)
+                    {
+                        MessageBox.Show("Username já existe.", "Aviso",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (_editandoId == null)
+                    {
+                        db.Utilizadores.Add(new Utilizador
+                        {
+                            Nome = txtNome.Text.Trim(),
+                            Username = txtUtilizador.Text.Trim(),
+                            Password = txtSenha.Text
+                        });
+                    }
+                    else
+                    {
+                        var u = db.Utilizadores.Find(_editandoId);
+                        u.Nome = txtNome.Text.Trim();
+                        u.Username = txtUtilizador.Text.Trim();
+                        u.Password = txtSenha.Text;
+                    }
+
+                    db.SaveChanges();
+                }
+
+                Limpar();
+                CarregarUtilizadores();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message, "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+
+            _editandoId = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            txtNome.Text = dataGridView1.CurrentRow.Cells[1].Value?.ToString();
+            txtUtilizador.Text = dataGridView1.CurrentRow.Cells[2].Value?.ToString();
+            txtSenha.Text = "";  // limpar - utilizador reintroduz a password
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow == null) return;
+
+            if (MessageBox.Show("Eliminar este utilizador?", "Confirmar",
+                MessageBoxButtons.YesNo) != DialogResult.Yes) return;
+
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            try
+            {
+                using (var db = new AppDbContext())
+                {
+                    var u = db.Utilizadores.Find(id);
+                    db.Utilizadores.Remove(u);
+                    db.SaveChanges();
+                }
+                Limpar();
+                CarregarUtilizadores();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message, "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Limpar()
+        {
+            txtNome.Text = "";
+            txtUtilizador.Text = "";
+            txtSenha.Text = "";
+            _editandoId = null;
+            dataGridView1.ClearSelection();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
